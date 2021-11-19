5d440d66700dfc7f5bf65660225addfc08519819
jet1125 <9811hjy@gmail.com>
2021-11-19 17:07:17 +0900

    commit

---
+++ combine/combine.py
@@ -0,0 +1,209 @@
+# USAGE
+# python detect_mask_video.py
+
+# import the necessary packages
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import os
+
+from yolo import YOLO
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-n', '--network', default="normal", choices=["prn", "tiny", "prn", "v4-tiny"],
+                help='Network Type')
+ap.add_argument('-d', '--device', type=int, default=0, help='Device to use')
+ap.add_argument('-s', '--size', default=416, help='Size for yolo')
+ap.add_argument('-c', '--confidence', default=0.2, help='Confidence for yolo')
+ap.add_argument('-nh', '--hands', default=-1, help='Total number of hands to be detected per frame (-1 for all)')
+args = ap.parse_args()
+
+if args.network == "normal":
+    print("loading yolo...")
+    yolo = YOLO("models/cross-hands.cfg", "models/cross-hands.weights", ["glove"])
+elif args.network == "prn":
+    print("loading yolo-tiny-prn...")
+    yolo = YOLO("models/cross-hands-tiny-prn.cfg", "models/cross-hands-tiny-prn.weights", ["hand"])
+elif args.network == "v4-tiny":
+    print("loading yolov4-tiny-prn...")
+    yolo = YOLO("models/cross-hands-yolov4-tiny.cfg", "models/cross-hands-yolov4-tiny.weights", ["hand"])
+else:
+    print("loading yolo-tiny...")
+    yolo = YOLO("models/cross-hands-tiny.cfg", "models/cross-hands-tiny.weights", ["hand"])
+
+yolo.size = int(args.size)
+yolo.confidence = float(args.confidence)
+
+def detect_and_predict_mask(frame, faceNet, maskNet):
+	# grab the dimensions of the frame and then construct a blob
+	# from it
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+		(104.0, 177.0, 123.0))
+
+	# pass the blob through the network and obtain the face detections
+	faceNet.setInput(blob)
+	detections = faceNet.forward()
+
+	# initialize our list of faces, their corresponding locations,
+	# and the list of predictions from our face mask network
+	faces = []
+	locs = []
+	preds = []
+
+	# loop over the detections
+	for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with
+		# the detection
+		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the confidence is
+		# greater than the minimum confidence
+		if confidence > 0.5:	
+			# compute the (x, y)-coordinates of the bounding box for
+			# the object
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			# ensure the bounding boxes fall within the dimensions of
+			# the frame
+			(startX, startY) = (max(0, startX), max(0, startY))
+			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+			# extract the face ROI, convert it from BGR to RGB channel
+			# ordering, resize it to 224x224, and preprocess it
+			face = frame[startY:endY, startX:endX]
+			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+			face = cv2.resize(face, (224, 224))
+			face = img_to_array(face)
+			face = preprocess_input(face)
+
+			# add the face and bounding boxes to their respective
+			# lists
+			faces.append(face)
+			locs.append((startX, startY, endX, endY))
+
+	# only make a predictions if at least one face was detected
+	if len(faces) > 0:
+		# for faster inference we'll make batch predictions on *all*
+		# faces at the same time rather than one-by-one predictions
+		# in the above `for` loop
+		faces = np.array(faces, dtype="float32")
+		preds = maskNet.predict(faces, batch_size=32)
+
+	# return a 2-tuple of the face locations and their corresponding
+	# locations
+	return (locs, preds)
+
+# construct the argument parser and parse the arguments
+
+# load our serialized face detector model from disk
+print("[INFO] loading face detector model...")
+prototxtPath = os.path.sep.join(["face_detector/", "deploy.prototxt"])
+weightsPath = os.path.sep.join(["face_detector/", "res10_300x300_ssd_iter_140000.caffemodel"])
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+# load the face mask detector model from disk
+print("[INFO] loading face mask detector model...")
+maskNet = load_model("mask_detector.model")
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+#vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+print("starting webcam...")
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(args.device)
+if vc.isOpened():  # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+# loop over the frames from the video stream
+image_process = False
+wait = False
+while rval:
+    key = cv2.waitKey(10)
+    if key == ord("s"):
+	    image_process = True
+
+    frame = imutils.resize(frame, width=400)
+    if image_process == True and wait == False:
+        print("a")
+        width, height, inference_time, results = yolo.inference(frame)
+        (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+    # display fps
+        cv2.putText(frame, f'{round(1/inference_time,2)} FPS', (15,15), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0,255,255), 2)
+
+    # sort by confidence
+        results.sort(key=lambda x: x[2])
+
+    # how many hands should be shown
+        hand_count = len(results)
+        if args.hands != -1:
+          hand_count = int(args.hands)
+
+    # display hands
+        for detection in results[:hand_count]:
+            id, name, confidence, x, y, w, h = detection
+            cx = x + (w / 2)
+            cy = y + (h / 2)
+
+        # draw a bounding box rectangle and label on the image
+            color = (0, 255, 255)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            text = "%s (%s)" % (name, round(confidence, 2))
+            cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5, color, 2)
+
+
+
+	# detect faces in the frame and determine if they are wearing a
+	# face mask or not
+        if args.hands != -1:
+            hand_count = int(args.hands)
+
+        for (box, pred) in zip(locs, preds):
+            (startX, startY, endX, endY) = box
+            (mask, withoutMask) = pred
+
+            # determine the class label and color we'll use to draw
+            # the bounding box and text
+            label = "Mask" if mask > withoutMask else "No Mask"
+            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+            # include the probability in the label
+            label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+            # display the label and bounding box rectangle on the output
+            # frame
+            cv2.putText(frame, label, (startX, startY - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+        wait = True
+        #cv2.imshow("preview", frame)
+    key1 = cv2.waitKey(10)
+    if key1 == ord("c"):
+        image_process = False
+        wait = False
+    
+    cv2.imshow("preview", frame)
+    if image_process == False:
+        rval, frame = vc.read()
+
+    key2 = cv2.waitKey(10)
+    if key2 == 27:  # exit on ESC
+        break
+
+
+
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+#vs.stop()
