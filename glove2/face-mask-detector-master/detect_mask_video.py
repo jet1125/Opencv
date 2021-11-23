9736bfd24147e6d06717384652db61e86e290aa9
jet1125 <9811hjy@gmail.com>
2021-11-23 10:34:20 +0900

    a

--- glove2/face-mask-detector-master/detect_mask_video.py
+++ glove2/face-mask-detector-master/detect_mask_video.py
@@ -152,7 +152,7 @@ while True:
 		# unpack the bounding box and predictions
 		(startX, startY, endX, endY) = box
 		(mask, withoutMask) = pred
-
+		print(mask, withoutMask)
 		# determine the class label and color we'll use to draw
 		# the bounding box and text
 		label = "Mask" if mask > withoutMask else "No Mask"
